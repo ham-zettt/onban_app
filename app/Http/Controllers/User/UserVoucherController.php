0289341e0792f0bb85ed7a1e001f7c691a00c243
abdulrahemfaqih <faqih3935@gmail.com>
2024-05-21 20:27:25 +0700

    get position worker:trigger checkbox

--- app/Http/Controllers/User/UserVoucherController.php
+++ app/Http/Controllers/User/UserVoucherController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\Voucher;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserVoucherController extends Controller
 {
     public function index() {
+
+        $allVoucher = Voucher::all();
         return view('user.voucher', [
             "title" => "Voucher",
             "nama" => session('userData')->customer->nama,
-            "role" => session('userData')->role
+            "role" => session('userData')->role,
+            "vouchers" => $allVoucher
+
         ]);
     }
 }
