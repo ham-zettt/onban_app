77de568861358d3954641be5e6eebef0ae5ebeff
mochammad hanafi <mco908472@gmail.com>
2024-05-21 21:46:47 +0700

    fix controller Chat

--- resources/views/user/order/find-worker.blade.php
+++ resources/views/user/order/find-worker.blade.php
@@ -37,7 +37,7 @@
                     <a href="{{ route('home') }}" id="cancel-link" class=" my-auto hover:text-gray-200 hover:border-gray-200 border-white border-2 mx-auto text-white p-1 rounded-md">Batalkan</a>
                 </div>
                 <div class="flex justify-center content-center h-1/2">
-                    <a href="{{route('chat')}}" class="my-auto hover:text-gray-200 hover:border-gray-200 border-white border-2 mx-auto text-white p-1 rounded-md">Chat Worker</a>
+                    <a href="{{route('userChat')}}" class="my-auto hover:text-gray-200 hover:border-gray-200 border-white border-2 mx-auto text-white p-1 rounded-md">Chat Worker</a>
                 </div>
                 <div class="flex justify-center content-center h-1/2">
                     <a href="#" class="my-auto hover:text-orange-400 hover:bg-gray-200 bg-white hover:border-orange-400 border-primary border-2 mx-auto text-primary p-1 rounded-md">Pembayaran</a>
