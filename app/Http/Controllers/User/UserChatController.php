18f566cc5609825d87f6b8c4bc9a796bcf736499
abdulrahemfaqih <faqih3935@gmail.com>
2024-05-21 22:40:37 +0700

    fix bug route Chat

--- app/Http/Controllers/User/UserChatController.php
+++ app/Http/Controllers/User/UserChatController.php
@@ -1,17 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
-use App\Http\Controllers\Controller;
+namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class UserChatController extends Controller
 {
-    public function index() {
-        return view('user.userChat', [
-            "title" => "userChat",
-            "nama" => session('userData')->customer->nama,
-            "role" => session('userData')->role
-        ]);
-    }
+    
 }
